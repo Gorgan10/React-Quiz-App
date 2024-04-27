461133516fc05b55f43156b4ff073380a08b05c1
Gorgan10 <gorgan15@mail.ru>
2024-04-26 18:26:05 -0700

    Finished forms and validations

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,10 +1,135 @@
 import React from 'react';
+import cl from './Auth.module.css'
+import Button from '../../components/UI/Button/Button';
+import Input from '../../components/UI/Input/Input';
 
 class Auth extends React.Component {
+  state = {
+    isFormValid: false,
+    formControls: {
+      email: {
+        value: '',
+        type: 'email',
+        label: 'Email',
+        errorMessage: 'Invalid Email',
+        valid: false,
+        touched: false,
+        rules: {
+          required: true,
+          email: true
+        }
+      },
+      password: {
+        value: '',
+        type: 'password',
+        label: 'Password',
+        errorMessage: 'Invalid Password',
+        valid: false,
+        touched: false,
+        rules: {
+          required: true,
+          minLength: 6
+        }
+      }
+    }
+  }
+
+  loginHandler = () => {
+
+  }
+  registerHandler = () => {
+
+  }
+
+  submitHandler = e => {
+    e.preventDefault()
+  }
+
+  validateControl = (value, rules) => {
+    if (!rules) {
+      return true
+    }
+
+    let isValid = true
+    if (rules.required && isValid) {
+      isValid = value.trim() !== ''
+    }
+
+    if (rules.email && isValid) {
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      isValid = emailRegex.test(value);
+    }
+
+    if (rules.minLength && isValid) {
+      isValid = value.length >= rules.minLength
+    }
+
+    return isValid
+  }
+
+  onChangeHandler = (event, controlName) => {
+    const { value } = event.target;
+
+    this.setState(prevState => {
+      const updatedControls = {
+        ...prevState.formControls,
+        [controlName]: {
+          ...prevState.formControls[controlName],
+          value,
+          touched: true,
+          valid: this.validateControl(value, prevState.formControls[controlName].rules)
+        }
+      };
+
+      let isFormValid = true;
+      for (let control in updatedControls) {
+        isFormValid = updatedControls[control].valid && isFormValid;
+      }
+
+      return {
+        formControls: updatedControls,
+        isFormValid: isFormValid
+      };
+    });
+  }
+
+  renderInput = () => {
+    return Object.keys(this.state.formControls).map((ctrlName, index) => {
+      const control = this.state.formControls[ctrlName]
+      return (
+        <Input
+          key={ctrlName + index}
+          label={control.label}
+          type={control.type}
+          value={control.value}
+          valid={control.valid}
+          touched={control.touched}
+          errorMessage={control.errorMessage}
+          shouldValidate={!!control.rules}
+          onChange={(e) => this.onChangeHandler(e, ctrlName)}
+        />
+      )
+    })
+  }
+
+
   render() {
     return (
-      <div>
-        <h1>Auth</h1>
+      <div className={cl.Auth}>
+        <div>
+          <h1>Log In</h1>
+
+          <form className={cl.Form} onSubmit={this.submitHandler}>
+
+            {this.renderInput()}
+
+
+            <div className={cl.buttons}>
+              <Button type='primary' onClick={this.loginHandler} disabled={!this.state.isFormValid}>Sign in</Button>
+              <Button type='success' onClick={this.registerHandler} disabled={!this.state.isFormValid}>Sign up</Button>
+            </div>
+          </form>
+        </div>
       </div>
     );
   }
--- src/containers/QuizCreator/QuizCreator.js
+++ src/containers/QuizCreator/QuizCreator.js
@@ -1,10 +1,163 @@
 import React from 'react';
+import cl from './QuizCreator.module.css'
+import Button from '../../components/UI/Button/Button';
+import {createControl, validateControl, validateForm} from '../../form/formFramework';
+import Input from '../../components/UI/Input/Input';
+import Select from '../../components/UI/Select/Select';
+
+function createOptionControl(id) {
+  return createControl(
+    {
+      label: `Option ${id}`,
+      placeholder: 'Type an option...',
+      id,
+      errorMessage: 'Required Field'
+    },
+    {
+      required: true
+    })
+}
+
+function createFormControl () {
+  return {
+    question: createControl({
+      label: 'Question',
+      placeholder: 'Type a question...',
+      errorMessage: 'Required Field'
+    }, {required: true}),
+    option1: createOptionControl(1),
+    option2: createOptionControl(2),
+    option3: createOptionControl(3),
+    option4: createOptionControl(4)
+  }
+}
 
 class QuizCreator extends React.Component {
+  state = {
+    quiz: [],
+    rightAnswerId: 1,
+    isFormValid: false,
+    formControls: createFormControl()
+  }
+
+  submitHandler = (e) => {
+    e.preventDefault()
+  }
+
+  createQuestionHandler = (event) => {
+    const quiz = this.state.quiz.concat()
+    const index = quiz.length + 1
+
+    const {question, option1, option2, option3, option4} = this.state.formControls
+
+    const questionItem = {
+      question: question.value,
+      id: index,
+      options: [
+        {text: option1.value, id: option1.id},
+        {text: option2.value, id: option2.id},
+        {text: option3.value, id: option3.id},
+        {text: option4.value, id: option4.id}
+      ]
+    }
+
+    quiz.push(questionItem)
+
+    this.setState({
+      quiz,
+      formControls: createFormControl(),
+      isFormValid: false,
+      rightAnswerId: 1
+    })
+  }
+
+  createQuizHandler = (event) => {
+    console.log(this.state.quiz)
+  }
+
+  onChangeHandler = (event, controlKey, rules) => {
+    const {value} = event.target
+
+    this.setState(prevState => {
+      const updatedControls = {
+        ...prevState.formControls,
+        [controlKey]: {
+          ...prevState.formControls[controlKey],
+          value,
+          touched: true,
+          valid: validateControl(value, rules)
+        }
+      }
+
+      return {
+        formControls: updatedControls,
+        isFormValid: validateForm(updatedControls)
+      }
+    })
+  }
+
+  renderControls = (formControls) => {
+    return Object.keys(formControls).map((controlKey, index) => {
+      const control = formControls[controlKey]
+      const key = `${controlKey}-${index}`
+
+      return (
+        <React.Fragment key={key}>
+          <Input
+            labelStyle={{color: '#000'}}
+            placeholder={control.placeholder}
+            key={index}
+            label={control.label}
+            errorMessage={control.errorMessage}
+            value={control.value}
+            touched={control.touched}
+            valid={control.valid}
+            shouldValidate={!!control.rules}
+            rules={control.rules}
+            onChange={(e) => this.onChangeHandler(e, controlKey, control.rules)}
+          />
+          {index === 0 ? <hr key={key} /> : undefined}
+        </React.Fragment>
+      )
+    })
+  }
+
+  selectChangeHandler = (value) => {
+    this.setState({
+      rightAnswerId: +value
+    })
+  }
+
   render() {
+    const select = <Select
+      label={'Select correct answer'}
+      value={this.state.rightAnswerId}
+      onChange={e => this.selectChangeHandler(e.target.value)}
+      options={[
+        {text: 1, value: 1},
+        {text: 2, value: 2},
+        {text: 3, value: 3},
+        {text: 4, value: 4}
+      ]}
+    />
+
     return (
-      <div>
-        <h1>QUIZ Creator</h1>
+      <div className={cl.QuizCreator}>
+        <div>
+          <h1>Create a Quiz</h1>
+
+          <form onSubmit={this.submitHandler}>
+
+            {this.renderControls(this.state.formControls)}
+
+            {select}
+
+            <div className={cl.buttons}>
+              <Button type='primary' disabled={!this.state.isFormValid} onClick={(event) => this.createQuestionHandler(event)}>Add the Question</Button>
+              <Button type='success' disabled={this.state.quiz.length === 0} onClick={this.createQuizHandler}>Create the Quiz</Button>
+            </div>
+          </form>
+        </div>
       </div>
     );
   }
